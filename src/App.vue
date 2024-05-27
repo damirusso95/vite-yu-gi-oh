ead7caafb39af685294cc2f8dd4ac221bad34e6c
damirusso95 <damirusso95@gmail.com>
2024-05-27 15:30:26 +0200

    M1: bozza header-new components con array per le carte

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
-<script setup>
+<script>
 
+
+// IMPORT HEADER
+import AppHeader from './components/AppHeader.vue';
+export default {
+  // CREO UNA 'LISTA' DI COMPONENTI
+  components: {
+    AppHeader,
+
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  }
+}
 </script>
 
+
 <template>
-  
- 
+<!-- RICHIAMO HEADER -->
+<AppHeader/>
+
 </template>
 
-<style >
-
+<style>
 </style>
+
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <div class="col-12">
+            <div class="row">
+                <div class="col-1">
+                    logo                   
+                </div>
+                <div class="col-2">
+                    <h1>YU GI OH!</h1>
+                </div>
+            </div>
+
+        </div>
+
+
+
+    </header>
+</template>
+
+
+<style></style>
